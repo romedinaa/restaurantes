f9194f36d99c4460184c9de7b2d8f8c99b3474be
romedinaa <31547213+romedinaa@users.noreply.github.com>
2021-03-15 15:24:17 -0600

    Add files via upload

---
+++ ProyectoLab/ProyectoLab/ProyectoLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoLabApp.swift
+//  ProyectoLab
+//
+//  Created by Roberta Medina on 15/03/21.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
