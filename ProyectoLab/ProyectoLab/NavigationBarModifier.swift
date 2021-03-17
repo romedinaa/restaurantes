8e9b401399f405926b03af52df4d5033cfc44ec0
romedinaa <robertamedina98@gmail.com>
2021-03-16 22:22:29 -0600

    rob

---
+++ ProyectoLab/ProyectoLab/NavigationBarModifier.swift
@@ -0,0 +1,50 @@
+//
+//  NavigationBarModifier.swift
+//  ProyectoLab
+//
+//  Created by Roberta Medina on 16/03/21.
+//
+
+import SwiftUI
+
+struct NavigationBarModifier: ViewModifier {
+        
+    var backgroundColor: UIColor?
+    
+    init( backgroundColor: UIColor?) {
+        self.backgroundColor = backgroundColor
+        let coloredAppearance = UINavigationBarAppearance()
+        coloredAppearance.configureWithTransparentBackground()
+        coloredAppearance.backgroundColor = .clear
+        coloredAppearance.titleTextAttributes = [.foregroundColor: UIColor(Color("DraculaOrchid"))]
+        coloredAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor(Color("DraculaOrchid"))]
+        
+        UINavigationBar.appearance().standardAppearance = coloredAppearance
+        UINavigationBar.appearance().compactAppearance = coloredAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = coloredAppearance
+        UINavigationBar.appearance().tintColor = UIColor(Color("DraculaOrchid"))
+
+    }
+    
+    func body(content: Content) -> some View {
+        ZStack{
+            content
+            VStack {
+                GeometryReader { geometry in
+                    Color(self.backgroundColor ?? .clear)
+                        .frame(height: geometry.safeAreaInsets.top)
+                        .edgesIgnoringSafeArea(.top)
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+extension View {
+ 
+    func navigationBarColor(_ backgroundColor: UIColor?) -> some View {
+        self.modifier(NavigationBarModifier(backgroundColor: backgroundColor))
+    }
+
+}
