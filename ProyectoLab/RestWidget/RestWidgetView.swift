059081f3fe62f2e6b0c89c39073e4b2829c4cc72
romedinaa <robertamedina98@gmail.com>
2021-06-12 20:31:47 -0500

    widget

---
+++ ProyectoLab/RestWidget/RestWidgetView.swift
@@ -0,0 +1,33 @@
+//
+//  RestVidgetView.swift
+//  RestWidgetExtension
+//
+//  Created by Roberta Medina on 12/06/21.
+//
+
+import SwiftUI
+import WidgetKit
+
+struct RestWidgetView: View {
+
+  let restDetails: RestDetailView
+    var rest: RestInfo
+    
+    
+
+  var body: some View {
+    ZStack {
+      Color(UIColor.systemIndigo)
+      VStack {
+        Text(rest.name)
+          .font(.system(size: 56))
+        Text(rest.location)
+          .font(.headline)
+          .multilineTextAlignment(.center)
+          .padding(.top, 5)
+          .padding([.leading, .trailing])
+          .foregroundColor(.white)
+      }
+    }
+  }
+}
